b1cdecd559449de582bf959e7e1b653e3610a809
nicolasrsande <nicolasrsande@gmail.com>
2020-11-18 18:24:51 -0300

    update urls

--- lib/afipwebservices/constants.rb
+++ lib/afipwebservices/constants.rb
@@ -3,8 +3,8 @@ module AfipWebservices
   URLS = {
     test: { wsaa: 'https://wsaahomo.afip.gov.ar/ws/services/LoginCms?WSDL',
             wsfe: 'https://wswhomo.afip.gov.ar/wsfev1/service.asmx?WSDL' },
-    production: { wsaa: 'https://wsaa.afip.gov.ar/ws/services/LoginCms',
-                  wsfe: 'https://servicios1.afip.gov.ar/wsfev1/service.asmx?WSDL'}
+    production: { wsaa: 'https://wsaa.afip.gov.ar/ws/services/LoginCms?WSDL',
+                  wsfe: 'https://servicios1.afip.gov.ar/wsfev1/service.asmx?WSDL' }
   }.freeze
   
   VALID_DOCUMENT_TYPES = [80, 86, 87, 89, 90, 91, 92, 93, 95, 96, 94, 99].freeze
